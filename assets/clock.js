8a262334aedec00b79de5776e8bd0b46773d29e5
Ricky Kurniawan <rkurniawan@hacktiv8.com>
2020-04-01 17:05:14 +0700

    Updated analog clock with better tutorials from WebDevSimplified in YouTube

--- assets/clock.js
+++ assets/clock.js
@@ -1,18 +1,22 @@
-const secDiv = document.getElementById('sec');
-const minDiv = document.getElementById('min');
-const hourDiv = document.getElementById('hour');
+setInterval(setClock, 1000);
 
-setInterval(updateClock, 1000);
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
 
-function updateClock(){
-	let date = new Date();
-	let sec = date.getSeconds() / 60;
-	let min = (date.getMinutes() + sec) / 60;
-	let hour = ( date.getHours() + min) / 12;
-	
-	secDiv.style.transform = "rotate(" + (sec * 360) + "deg)";
-	minDiv.style.transform = "rotate(" + (min * 360) + "deg)";
-	hourDiv.style.transform = "rotate(" + (hour * 360) + "deg)";
+function setClock(){
+	const currentDate = new Date();
+	const sec = currentDate.getSeconds() / 60;
+	const min = (currentDate.getMinutes() + sec) / 60;
+	const hour = ( currentDate.getHours() + min) / 12;
+
+	setRotation(secondHand, sec);
+	setRotation(minuteHand, min);
+	setRotation(hourHand, hour);
 }
 
-updateClock();
+function setRotation(element, rotationRatio) {
+	element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock(); //this to avoid jumping the clock hands from 12 to current time
